2528672fe3fb9e836ecee93583229f11ab511856
konrad.oglaza <konrad.oglaza@schibsted.com>
2024-10-18 12:37:01 +0200

    Lab02

---
+++ Lab02/WebApplication1/Controllers/CarController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CarViewModel viewModel)
+        {
+            this.ViewBag.Message = $"{viewModel.Brand} {viewModel.Model} added to favorites!";
+            return View("Info", viewModel);
+        }
+        [HttpGet]
+        public IActionResult List()
+        {
+            List<CarViewModel> list = new List<CarViewModel>();
+            list.Add(new CarViewModel() { Brand="Kia", Model="Ceed", TopSpeed=180 });
+            list.Add(new CarViewModel() { Brand = "Fiat", Model = "500", TopSpeed = 190 });
+            list.Add(new CarViewModel() { Brand = "BMW", Model = "X5", TopSpeed = 250 });
+            return View(list);
+        }
+    }
+}
---
+++ Lab02/WebApplication1/Models/CarViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class CarViewModel
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int TopSpeed { get; set; }
+
+    }
+}
