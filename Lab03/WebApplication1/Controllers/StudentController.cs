987fc2a49df871d063826c7be85f2c88a7d67c7b
konrad.oglaza <konrad.oglaza@schibsted.com>
2024-11-02 10:21:42 +0100

    lab03

---
+++ Lab03/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        static List<StudentViewModel> students = new List<StudentViewModel>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(StudentViewModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                // data validated
+                student.Id = students.Max(x => x.Id) + 1;
+                students.Add(student);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(); // go back to the form with errors
+            }
+        }
+    }
+}
