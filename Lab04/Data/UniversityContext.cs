c7100fc2594c63321a224153cae8c810c183c775
konrad.oglaza <konrad.oglaza@schibsted.com>
2024-11-25 18:33:39 +0100

    lab04 full 2

---
+++ Lab04/Data/UniversityContext.cs
@@ -0,0 +1,34 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext()
+        {
+        }
+        public DbSet<StudentEntity> Students { get; set; }
+        public DbSet<CourseEntity> Courses { get; set; }
+        public DbSet<InstructorEntity> Instructors { get; set; }
+        public DbSet<EnrollmentEntity> Enrollments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            var db = System.IO.Path.Join(path, "university.db");
+            options.UseSqlite($"Data Source={db}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<InstructorEntity>().HasData(
+                new InstructorEntity() { Id = 1, Name = "Konrad Ogłaza", AcademicTitle = "mgr inż." }
+            );
+            modelBuilder.Entity<CourseEntity>().HasData(
+                new CourseEntity() { Id = 1, Name = "ASP.NET", Credits = 10, InstructorId = 1 }
+            );
+        }
+    }
+}
---
+++ Lab04/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private IStudentService _service;
+        public StudentController(IStudentService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_service.FindAll());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(StudentViewModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                // data validated
+
+                _service.Add(student);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(); // go back to the form with errors
+            }
+        }
+    }
+}
